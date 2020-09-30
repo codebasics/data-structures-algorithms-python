7dcd3bfd30826d09b9ef1a655577b6d8d3faf677
techshivang <44114166+techshivang@users.noreply.github.com>
2020-10-01 00:49:08 +0530

    Complete the program of odd_even.py by adding the functionality for even Numbers.
    As the Program name was odd_even but logic define in the Program is only for the odd numbers so i simply create a empty list and append all the even values in the list.

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -1,9 +1,13 @@
 max = int(input("Enter max number: "))
 
 odd_numbers = []
+even_numbers=[]
 
 for i in range(max):
     if i%2 == 1:
         odd_numbers.append(i)
+    else:
+        even_numbers.append(i)
 
 print("Odd numbers: ",odd_numbers)
+print("Even numbers:",even_numbers)
