aa41e6545504826b98d7f8e29e6c06022c509159
Master Amitoj <universaldistructor@gmail.com>
2020-10-25 16:40:47 +0530

    Changed to snake_case

--- algorithms/6_ShellSort/shell_sort_exercise_solution.py
+++ algorithms/6_ShellSort/shell_sort_exercise_solution.py
@@ -3,20 +3,20 @@ def shell_sort(arr):
     div = 2
     gap = n//div
     while gap > 0:
-        IndexToDelete = []
+        index_to_delete = []
         for i in range(gap, n):
             temp = arr[i]
             j = i
             while j >= gap and arr[j-gap] >= temp:
                 if arr[j-gap] == temp:
-                    IndexToDelete.append(j)
+                    index_to_delete.append(j)
                 arr[j] = arr[j-gap]
                 j -= gap
             arr[j] = temp
-        IndexToDelete=list(set(IndexToDelete))
-        IndexToDelete.sort()
-        if IndexToDelete:
-            for i in IndexToDelete[-1::-1]:
+        index_to_delete=list(set(index_to_delete))
+        index_to_delete.sort()
+        if index_to_delete:
+            for i in index_to_delete[-1::-1]:
                 del arr[i]
         div *= 2
         n = len(arr)
