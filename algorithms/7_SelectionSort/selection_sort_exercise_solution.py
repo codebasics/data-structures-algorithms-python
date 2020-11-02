ae2863ee96116619ce3f7e83b172849ee36f1206
Master Amitoj <universaldistructor@gmail.com>
2020-11-02 15:54:25 +0530

    removed extra data

--- algorithms/7_SelectionSort/selection_sort_exercise_solution.py
+++ algorithms/7_SelectionSort/selection_sort_exercise_solution.py
@@ -11,20 +11,20 @@ def multilevel_selection_sort(elements, sort_by_list):
 
 if __name__ == '__main__':
     elements = [
-        {'First Name': 'Raj', 'Last Name': 'Nayyar', 'Middle Name': 'B'},
-        {'First Name': 'Suraj', 'Last Name': 'Sharma', 'Middle Name': ''},
-        {'First Name': 'Karan', 'Last Name': 'Kumar', 'Middle Name': ''},
-        {'First Name': 'Jade', 'Last Name': 'Canary', 'Middle Name': ''},
-        {'First Name': 'Raj', 'Last Name': 'Thakur', 'Middle Name': 'A'},
-        {'First Name': 'Raj', 'Last Name': 'Sharma', 'Middle Name': 'A'},
-        {'First Name': 'Kiran', 'Last Name': 'Kamla', 'Middle Name': ''},
-        {'First Name': 'Armaan', 'Last Name': 'Kumar', 'Middle Name': ''},
-        {'First Name': 'Jaya', 'Last Name': 'Sharma', 'Middle Name': ''},
-        {'First Name': 'Ingrid', 'Last Name': 'Galore', 'Middle Name': ''},
-        {'First Name': 'Jaya', 'Last Name': 'Seth', 'Middle Name': ''},
-        {'First Name': 'Armaan', 'Last Name': 'Dadra', 'Middle Name': ''},
-        {'First Name': 'Ingrid', 'Last Name': 'Maverick', 'Middle Name': ''},
-        {'First Name': 'Aahana', 'Last Name': 'Arora', 'Middle Name': ''}
+        {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+        {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+        {'First Name': 'Karan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jade', 'Last Name': 'Canary'},
+        {'First Name': 'Raj', 'Last Name': 'Thakur'},
+        {'First Name': 'Raj', 'Last Name': 'Sharma'},
+        {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+        {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+        {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+        {'First Name': 'Jaya', 'Last Name': 'Seth'},
+        {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+        {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+        {'First Name': 'Aahana', 'Last Name': 'Arora'}
     ]
 
     print(f'Given unsorted array:', *elements, sep='\n')
