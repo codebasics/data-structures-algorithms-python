a918f5f3ddcaa006be7b65198410fd435c7be7de
Master Amitoj <universaldistructor@gmail.com>
2020-11-02 15:12:22 +0530

    Changed multilevel_sort Function to Generalized

--- algorithms/7_SelectionSort/selection_sort_exercise_solution.py
+++ algorithms/7_SelectionSort/selection_sort_exercise_solution.py
@@ -1,22 +1,39 @@
-def multilevel_selection_sort(elements):
+def multilevel_selection_sort(elements,sort_by):
+    set_of_indices=set()
+    for x in range(len(elements)):
+        for kx in (elements[x].keys()):
+            set_of_indices.add(kx)
+    
+    #
+    print(set_of_indices)
+    #
+
+    set_of_indices.remove(sort_by)
+
+    #
+    print(set_of_indices)
+    #
+
     for x in range(len(elements)):
         min_index = x
         for y in range(x, len(elements)):
-            if elements[y]['First Name'] < elements[min_index]['First Name']:
+            if elements[y][sort_by] < elements[min_index][sort_by]:
                 min_index = y  
         if x != min_index:
             elements[x], elements[min_index] = elements[min_index], elements[x]
-    list_of_firstnames=list(set([i['First Name'] for i in elements]))
-    for firstname in list_of_firstnames:
-        for x in range(len(elements)):
-            if elements[x]['First Name']==firstname:
-                min_index = x
-                for y in range(x, len(elements)):
-                    if elements[y]['First Name']==firstname:
-                        if elements[y]['Last Name'] < elements[min_index]['Last Name']:
-                            min_index = y  
-                if x != min_index:
-                    elements[x], elements[min_index] = elements[min_index], elements[x]
+    for indx in set_of_indices:
+        list_of_sortby=list(set([i[sort_by] for i in elements]))
+        for this_of_sort in list_of_sortby:
+            for x in range(len(elements)):
+                if elements[x][sort_by]==this_of_sort:
+                    min_index = x
+                    for y in range(x, len(elements)):
+                        if elements[y][sort_by]==this_of_sort:
+                            if elements[y][indx] < elements[min_index][indx]:
+                                min_index = y  
+                    if x != min_index:
+                        elements[x], elements[min_index] = elements[min_index], elements[x]
+        sort_by=indx
 
 if __name__ == '__main__':
     elements = [
@@ -37,7 +54,7 @@ if __name__ == '__main__':
     ]
 
     print(f'Given unsorted array:',*elements,sep='\n')
-    multilevel_selection_sort(elements)
+    multilevel_selection_sort(elements,'First Name')
     print(f'Array after Multi-Level Sorting:',*elements,sep='\n')
 
 
