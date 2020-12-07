8adbdedd2d2ea18d19cf06d19760c012fc02d616
Nikhil Ailani <46107121+nikhilailani@users.noreply.github.com>
2020-12-07 16:54:14 +0530

    Create bfs_exercise_solution.py

---
+++ algorithms/9_BreadthFirstSearch/bfs_exercise_solution.py
@@ -0,0 +1,27 @@
+def bfs(data, start, end, visited=[]):
+    queue = [start]
+    
+    while queue:
+        current_node = queue.pop(0)
+        if current_node==end:
+            print("Path exists!")
+            print("Path : " + "->".join(visited) + "->"+end)
+            return
+        visited.append(current_node)
+        
+        for i in data[current_node] - set(visited):
+            queue.append(i)
+    print("Path does not exist!")    
+    return
+        
+        
+
+        
+if __name__ == '__main__':
+  data = {'A': {'B'},
+        'B': {'C', 'D'},
+        'C': {'E'},
+        'D': {'E'},
+        'E': {'F'},
+        'F': set()}
+  bfs(data, 'A', 'D')
