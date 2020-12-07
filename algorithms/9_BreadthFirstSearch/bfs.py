ca1904fde9579be0fa2b244c41b42ea16b2b0d56
Nikhil Ailani <46107121+nikhilailani@users.noreply.github.com>
2020-12-07 16:50:50 +0530

    Update bfs.py

--- algorithms/9_BreadthFirstSearch/bfs.py
+++ algorithms/9_BreadthFirstSearch/bfs.py
@@ -1,24 +1,24 @@
-def bfs(Data, start, visited=set()):
+def bfs(data, start, visited=set()):
 
     queue = [start]
     
     while queue:
-        currentnode = queue.pop(0)
-        if currentnode not in visited: print(currentnode, end = " ")
-        visited.add(currentnode)
+        current_node = queue.pop(0)
+        if current_node not in visited: print(current_node, end = " ")
+        visited.add(current_node)
         
-        for i in Data[currentnode] - visited:
+        for i in data[current_node] - visited:
             queue.append(i)
             
     return
         
-        
-Data = {'A': {'B'},
+if __name__ == '__main__':
+    
+    data = {'A': {'B'},
         'B': {'A', 'C', 'D'},
         'C': {'B', 'E'},
         'D': {'B', 'E'},
         'E': {'C', 'D', 'F'},
         'F': {'E'}}
-        
-if __name__ == '__main__':
-    bfs(Data, 'A')
+    
+    bfs(data, 'A')
