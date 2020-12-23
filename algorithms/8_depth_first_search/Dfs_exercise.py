23a6cc984544cb8cb058f90aa6cbe1cda507afe6
d_p_beladiya <darshan@atliq.com>
2020-12-23 10:13:22 +0530

    flake 8 convention followed

--- algorithms/8_depth_first_search/Dfs_exercise.py
+++ algorithms/8_depth_first_search/Dfs_exercise.py
@@ -1,36 +1,29 @@
-#function for depth first search
-def dfs(data, start,employee,visited=set()):
-    
-    #if not visited print it
+# function for depth first search
+def dfs(data, start, employee, visited=set()):
+    # if not visited print it
     if start not in visited:
-        print(start,end=" ")
-        
-        if start==employee:
-            print(":",end=" ")
-        
-    
+        print(start, end=" ")
+        if start == employee:
+            print(":", end=" ")
     visited.add(start)
 
-
     for i in data[start] - visited:
-        
-        #if not visited go in depth of it
+        # if not visited go in depth of it
         dfs(data, i, visited)
-        
-    return 
+    return
 
 
-
-#sample data in dictionary form
-data = {"karan": {"darshan","nikhil"},
-        "darshan": {"khantil", "tanuj"},
-        'tanuj': {"nikhil"},
-        "krinish": {"hetul"},
-        "khantil" : set(),
-        "nikhil" : set()
-        }
+# sample data in dictionary form
+data = {
+    "karan": {"darshan", "nikhil"},
+    "darshan": {"khantil", "tanuj"},
+    'tanuj': {"nikhil"},
+    "krinish": {"hetul"},
+    "khantil": set(),
+    "nikhil": set()
+    }
 
 
 if __name__ == '__main__':
 
-    dfs(data, "karan","karan")
+    dfs(data, "karan", "karan")
--- algorithms/8_depth_first_search/dfs.py
+++ algorithms/8_depth_first_search/dfs.py
@@ -1,30 +1,26 @@
-#function for depth first search
+# function for depth first search
 def dfs(data, start, visited=set()):
-    
-    #if not visited print it
+
+    # if not visited print it
     if start not in visited:
-        print(start,end=" ")
-        
+        print(start, end=" ")
+
     visited.add(start)
 
-
     for i in data[start] - visited:
-        
-        #if not visited gi in depth of it
+
+        # if not visited gi in depth of it
         dfs(data, i, visited)
-        
-    return 
+    return
 
 
-
-#sample data in dictionary form
+# sample data in dictionary form
 data = {'A': {'B'},
         'B': {'A', 'C', 'D'},
         'C': {'B', 'E'},
         'D': {'B', 'E'},
         'E': {'C', 'D', 'F'},
         'F': {'E'}}
-
 
 
 if __name__ == '__main__':
