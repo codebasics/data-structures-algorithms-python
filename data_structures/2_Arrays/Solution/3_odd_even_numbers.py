311ec811c90283d80209e519bd6c45b9b7959ac6
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2021-08-29 12:16:04 +0530

    Update 3_odd_even_numbers.py
    updated the range function and replaced modulo with bitwise and (&)

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -2,8 +2,8 @@ max = int(input("Enter max number: "))
 
 odd_numbers = []
 
-for i in range(1, max):
-    if i % 2 == 1:
+for i in range(1, max+1):
+    if i & 1:
         odd_numbers.append(i)
 
 print("Odd numbers: ", odd_numbers)
