bfde83cde399f995df5098be50e54e179e3db47a
Ayush Gupta <79574192+iayushbhartiya@users.noreply.github.com>
2022-04-10 14:01:27 +0530

    Update 3_odd_even_numbers.py

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -1,7 +1,11 @@
 max = int(input("Enter max number: "))
 
 odd_numbers = []
-
+#if number will be odd in that case it will omit the last number for that
+if max % 2 == 0:
+    max = max
+else:
+    max += 1
 for i in range(1, max):
     if i % 2 == 1:
         odd_numbers.append(i)
